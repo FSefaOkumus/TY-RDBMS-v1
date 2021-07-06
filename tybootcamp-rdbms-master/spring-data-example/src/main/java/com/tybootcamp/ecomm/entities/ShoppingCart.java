e0c04538711c571c92202bbb552cdd82ef3a6c67
FSefaOkumus <62523196+FSefaOkumus@users.noreply.github.com>
2021-07-06 03:53:17 +0300

    Update ShoppingCart.java

--- tybootcamp-rdbms-master/spring-data-example/src/main/java/com/tybootcamp/ecomm/entities/ShoppingCart.java
+++ tybootcamp-rdbms-master/spring-data-example/src/main/java/com/tybootcamp/ecomm/entities/ShoppingCart.java
@@ -1,37 +1,40 @@
-package com.tybootcamp.ecomm.entities;
-
-import com.tybootcamp.ecomm.repositories.CartRepository;
-
-import javax.persistence.*;
-import java.util.List;
-
-public class ShoppingCart {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @OneToMany
-    @JoinColumn(name ="product_id")
-    private Product product;
-
-//    public List<Product> CartItems(Customer customer){
-//        return CartRepository.findByCustomer(customer);
+//package com.tybootcamp.ecomm.entities;
+//
+//
+//import javax.persistence.*;
+//import java.util.List;
+//
+//public class ShoppingCart {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    @OneToOne
+//    @JoinColumn(name ="customer_id")
+//    private Customer customer;
+//
+//    @OneToMany
+//    @JoinColumn(name ="product_id")
+//    private Product product;
+//
+////    public List<Product> CartItems(Customer customer){
+////        return CartRepository.findByCustomer(customer);
+////    }
+//
+//    public Long getId() {
+//        return id;
 //    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-}
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Product getProduct() {
+//        return product;
+//    }
+//
+//    public void setProduct(Product product) {
+//        this.product = product;
+//    }
+//}
