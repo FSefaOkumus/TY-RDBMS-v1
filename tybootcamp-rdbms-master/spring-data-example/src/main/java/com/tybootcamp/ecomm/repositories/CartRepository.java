c785be1cd924217c0eef86455684f8d113ba5d88
FSefaOkumus <62523196+FSefaOkumus@users.noreply.github.com>
2021-07-06 03:54:39 +0300

    Delete CartRepository.java

--- tybootcamp-rdbms-master/spring-data-example/src/main/java/com/tybootcamp/ecomm/repositories/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tybootcamp.ecomm.repositories;
-
-import com.tybootcamp.ecomm.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Customer,Long> {
-
-}
