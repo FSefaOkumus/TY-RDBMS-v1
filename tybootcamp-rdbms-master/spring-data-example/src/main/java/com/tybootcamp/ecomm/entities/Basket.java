176e9f57cc93588dfe21385761dc5863e6931728
FSefaOkumus <62523196+FSefaOkumus@users.noreply.github.com>
2021-07-06 03:52:06 +0300

    Update Basket.java

--- tybootcamp-rdbms-master/spring-data-example/src/main/java/com/tybootcamp/ecomm/entities/Basket.java
+++ tybootcamp-rdbms-master/spring-data-example/src/main/java/com/tybootcamp/ecomm/entities/Basket.java
@@ -2,6 +2,8 @@ package com.tybootcamp.ecomm.entities;
 
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Basket {
@@ -14,11 +16,54 @@ public class Basket {
     @JoinColumn(name ="customer_id")
     private Customer customer;
 
-    @OneToOne
-    @JoinColumn(name = "shoppingcart_id")
-    private ShoppingCart cart;
+    @OneToMany
+    @JoinColumn(name ="product_id")
+    private Product product;
 
-//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "seller", orphanRemoval = true)
 
-//    private Map<Ineger>
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public List<Product> Basket() {
+        return cartItems;
+    }
+
+    List<Product> cartItems= new ArrayList<Product>();
+
+
+    public void addItemtoCart(Product product){  //Products
+        cartItems.add(product);
+    }
+
+    public void removeItemfromCart(Product product){
+        cartItems.remove(product);
+    }
+
+    public void printCart(){
+        for (Product items : cartItems){
+            System.out.println(items.getName());
+        }
+    }
+    public void clearBasket(){
+        for (Product items : cartItems){
+            cartItems.remove(items);
+        }
+    }
+
+    public double totalPriceBasket(){
+        float totalPrice = 0;
+        for (Product items : cartItems){
+            totalPrice+=(items.getPrice());
+        }
+        return totalPrice;
+
+    }
+
+
 }
